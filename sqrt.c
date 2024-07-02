0c22735c82c19c2e9893bff3b0d54d9c796488d0
aidahrufai19 <aidahrufai19@gmail.com>
2024-07-02 12:35:33 +0530

    Add files via upload

---
+++ sqrt.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{double a,x;
+printf("Enter the no. you want to find square root of");
+scanf("%lf",&a);
+x=sqrt(a);
+printf("The squareroot of %lf is %lf",a,x);
+return 0;
+
+
+
+}
