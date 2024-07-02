6d347098b03996234b89399f2eae34ebf7149ce8
aidahrufai19 <aidahrufai19@gmail.com>
2024-07-02 12:17:45 +0530

    Add files via upload

---
+++ PI.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PI 3.14
+int main()
+{float r,parameter;
+printf("enter the radius");
+scanf("%f",&r);
+parameter=2*PI*r;
+printf("the parameter of the raduis %f is %f",r,parameter);
+return 0;
+}
