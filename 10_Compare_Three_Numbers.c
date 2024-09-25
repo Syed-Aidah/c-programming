068c4bd9beb4e52a9dd92d1f096e4c98af836655
Syed Aidah Rufai <aidahrufai19@gmail.com>
2024-09-25 11:48:13 +0530

    Update 10_Compare_Three_Numbers.c

--- 10_Compare_Three_Numbers.c
+++ 10_Compare_Three_Numbers.c
@@ -1,5 +1,4 @@
-//Use nesting of if-else (compare three numbers).
-
+Use nesting of if-else (compare three numbers).
 #include <stdio.h>
 
 int main() 
@@ -8,8 +7,41 @@ int main()
     
     printf("Enter three numbers: ");
     scanf("%d %d %d", &a,&b,&c);
-    
-    if (a > b) 
+    if(a==b)
+    {
+        if(a>c)
+        {
+            printf("the largest no. is %d",c);
+        }
+        if(c>a)
+        {
+            printf("%d is the largest no ",c);
+        }
+    }
+    else if(b==c)
+    {
+        if(b>a)
+        {
+            printf("The largest nos is %d",b);
+        }
+        if(a>b)
+        {
+            printf("The largest no is %d",a);
+        }
+    }
+    else if(a==c)
+    {
+        if(a>b)
+        {
+            printf("The largest no is %d",a);
+        }
+        if(b>a)
+        {
+            printf("The largest no is %d",b);
+        }
+        
+    }
+    else if (a > b) 
     {
         if (a > c) 
         {
@@ -20,7 +52,7 @@ int main()
             printf("The largest number is: %d\n", c);
         }
     } 
-    else 
+    else if(b>a)
     {
         if (b > c) 
         {
@@ -30,6 +62,10 @@ int main()
         {
             printf("The largest number is: %d\n", c);
         }
+    }
+    else if(a==b && b==c)
+    {
+        printf("All three no.s are equal");
     }
 
     return 0;
